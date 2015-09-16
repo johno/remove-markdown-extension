2a69a87a22bd14d0ddf0123a1fe2065c692f339a
John Otander <johnotander@gmail.com>
2015-09-15 18:53:58 -0600

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var assert = require('assert');
+var removeMarkdownExtension = require('..');
+
+describe('remove-markdown-extension', function() {
+
+  it('should do something awesome', function() {
+    assert.equal(removeMarkdownExtension(), true);
+  });
+});
